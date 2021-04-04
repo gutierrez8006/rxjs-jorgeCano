ab38c1407fefb0875f9e9b6d7dc4a6c5bb6f653d
Juan Carlos Gutierrez <gutierrez.8006@gmail.com>
2021-04-04 16:37:40 -0500

    CatchError

--- src/app/catch-error/catch-error.component.ts
+++ src/app/catch-error/catch-error.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-catch-error',
@@ -10,6 +12,22 @@ export class CatchErrorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    of('Welcome Gente', 'como estan?', 'Hola Gente')
+    .pipe(
+      map((v) => {
+        if (v === 'Hola Gente') {
+          throw new Error('Saludo incorrecto');
+        }
+        return v;
+      }),
+      catchError((err) => {
+        throw new Error('Error' + err);
+      })
+    ).subscribe(
+      (s) => console.log('Next: ' + s),
+      (err) => console.error('Err: ' + err)
+    );
+
   }
 
 }
